0ca3f52283fe94e039eda19f65bb75a4a56c83b4
Lauren Washington <consults@statsandrecs.com>
2017-01-02 16:42:15 -0800

    Update Summary_Statistics.R

--- Summary_Statistics.R
+++ Summary_Statistics.R
@@ -12,8 +12,8 @@ quantile(x, deciles)
 summary(x)
 summary(data)
 
-'''R will not skip missing values unless explicitly requested to do so.The summary statistic of a vector with an unknown
-value is unknown. You can give the na.rm argument (not available, remove) to request that missing values be removed'''
+#R will not skip missing values unless explicitly requested to do so.The summary statistic of a vector with an unknown
+#value is unknown. You can give the na.rm argument (not available, remove) to request that missing values be removed
 
 mean(x, na.rm = T)
 
