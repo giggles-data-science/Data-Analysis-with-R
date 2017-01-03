3acebd46490517020367e4a063faafbf77847c38
Lauren Washington <consults@statsandrecs.com>
2017-01-02 20:45:44 -0800

    Added Stripcharts

--- Boxplots.R
+++ Boxplots.R
@@ -22,3 +22,19 @@ boxplot(height ~ grade)
 
 #height for fifth graders paralle to height for eighth graders
 boxplot(height.fifth, height.eighth)
+
+        
+#Stripcharts
+#if the boxplot has a small interquartile range it may be more useful to plot the raw data points
+
+#mex setting reduces the interline distance
+#mar reduces the number of lines that surround the plot region
+#stack stacks identical values so they are more visible
+#jitter offsets points a random amount vertically and adjust the jitter to make it clearer
+attach(iris)
+opar <- par(mfrow=c(2,2), mex = 0.8, mar = c(3,3,2,1)+.1)
+stripchart(Petal.Width ~ Species)
+stripchart(Petal.Width ~ Species, method = "stack")
+stripchart(Petal.Width ~ Species, method = "jitter")
+stripchart(Petal.Width ~ Species, method = "jitter", jitter = .03)
+par(opar)
