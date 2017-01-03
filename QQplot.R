6fe3a2e4041e25f829917c4a9fd4d97e84bdd861
Lauren Washington <consults@statsandrecs.com>
2017-01-02 18:41:31 -0800

    Create QQplot.R

---
+++ QQplot.R
@@ -0,0 +1,5 @@
+#should expect to obtain a straight line if data is normally distributed
+#q-q plots stands for quantile vs quantile plots
+#the distribution has heavy tails if the outer parts of the curve are steeper than the middle part
+
+qqnorm(x)
