bd966b937583a656d3d493f84e0ac1c2b0905177
Lauren Washington <consults@statsandrecs.com>
2017-01-16 16:17:34 -0800

    Update Random-Sampling.R
    Added in replication

--- Random-Sampling.R
+++ Random-Sampling.R
@@ -9,3 +9,12 @@ sample(c("H","T"), 10, replace = T)
 #simulate data with nonequal probabilities by using the prob argument
 #simulate a sample of student test grades with an 80% chance of passing
 sample(c("pass", "fail"), 15, replace = T, prob=c(0.8,0.2))
+
+#sample 5000 user sessions 1000 times
+#visualize the distribution of means 
+dec_avg_pageviews <- replicate(1000, {
+  dec_avg_pageviews_reps <- df[sample(nrow(df), 5000, replace = FALSE, prob = NULL),]
+  mean(dec_avg_pageviews_reps$pageview_count)
+})
+hist(dec_avg_pageviews,  main = "Dec 2016; number of user sessions = 5,000")
+
